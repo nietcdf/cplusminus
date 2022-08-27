bc323fe697474fc8658704cf917e854a4a933fb8
EC2 Default User <ec2-user@ip-172-31-65-160.ec2.internal>
2022-08-27 16:02:01 +0000

    Add Rule class

--- Driver.cpp
+++ Driver.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "Rule.h"
 
 int main(){
     std::cout << "hello" << std::endl;
+    Rule* r = new Rule();
 }
---
+++ Rule.cpp
@@ -0,0 +1,5 @@
+#include "Rule.h"
+#include <iostream>
+Rule::Rule() {
+    std::cout << "Rule constructor" << std::endl;
+}
